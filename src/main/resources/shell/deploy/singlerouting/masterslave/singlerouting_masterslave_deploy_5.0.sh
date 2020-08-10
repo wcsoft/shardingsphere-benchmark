b514565a639db61b86e24b9d7a9d93dc03b59744
wcsoft <wcong86@163.com>
2020-08-10 17:52:16 +0800

    update deploy shell for 5.0

--- src/main/resources/shell/deploy/singlerouting/masterslave/singlerouting_masterslave_deploy_5.0.sh
+++ src/main/resources/shell/deploy/singlerouting/masterslave/singlerouting_masterslave_deploy_5.0.sh
@@ -2,7 +2,7 @@
 cd /home/jenkins
 ./apache-shardingsphere-*-shardingsphere-proxy-bin/bin/stop.sh
 sleep 5
-rm -f  /home/jenkins/apache-shardingsphere-*-sharding-proxy-bin/conf/config-*.yaml
+rm -f  /home/jenkins/apache-shardingsphere-*-shardingsphere-proxy-bin/conf/config-*.yaml
 cp -f /export/shardingsphere-benchmark/yaml_conf/singlerouting/masterslave/proxy/config-proxy-singlerouting-masterslave.yaml /export/shardingsphere-benchmark/yaml_conf/server.yaml /home/jenkins/apache-shardingsphere-*-shardingsphere-proxy-bin/conf
 ./apache-shardingsphere-*-shardingsphere-proxy-bin/bin/start.sh
 sleep 10
