8cc30f6b93783c9a798f0add98dde346be5f72e3
wcsoft <wcong86@163.com>
2020-08-13 18:10:24 +0800

    upadate yaml for 4.1.1

--- src/main/java/org/apache/shardingsphere/benchmark/jmeter/common/jdbc/encrypt/JMeterJDBCCommonEncryptInsert.java
+++ src/main/java/org/apache/shardingsphere/benchmark/jmeter/common/jdbc/encrypt/JMeterJDBCCommonEncryptInsert.java
@@ -3,19 +3,19 @@ package org.apache.shardingsphere.benchmark.jmeter.common.jdbc.encrypt;
 import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
 import org.apache.jmeter.samplers.SampleResult;
 import org.apache.shardingsphere.benchmark.db.jdbc.JDBCDataSourceUtil;
-import org.apache.shardingsphere.benchmark.db.shardingjdbc.ShardingConfigType;
-import org.apache.shardingsphere.benchmark.db.shardingjdbc.ShardingJDBCDataSourceFactory;
 import org.apache.shardingsphere.benchmark.jmeter.JMeterBenchmarkBase;
 
 import javax.sql.DataSource;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Random;
 
 public class JMeterJDBCCommonEncryptInsert extends JMeterBenchmarkBase {
 
     public static DataSource dataSource;
+    public Random r = new Random(1);
+    public int tableCount = Integer.valueOf((String)dbConfig.get("benchmark.table.count")).intValue();
 
     static {
         dataSource = JDBCDataSourceUtil.initDb((String) dbConfig.get("jdbc.benchmark.fullrouting.encrypt.ds0.datasource"),
@@ -34,8 +34,8 @@ public class JMeterJDBCCommonEncryptInsert extends JMeterBenchmarkBase {
         try {
             connection = dataSource.getConnection();
             String insertSql = (String) sqlConfig.get("common.jdbc.insert.sql");
-            List insertParams = convertParams((List) sqlConfig.get("common.jdbc.insert.values"));
-            insertRecords(connection, insertSql, insertParams);
+            List insertParams = convertParams((List) sqlConfig.get("common.jdbc.insert.values"), r.nextInt(tableCount));
+            JDBCDataSourceUtil.insert(connection, insertSql, insertParams);
         } catch (SQLException e) {
             results.setSuccessful(false);
             e.printStackTrace();
--- src/main/java/org/apache/shardingsphere/benchmark/jmeter/common/jdbc/sharding/JMeterJDBCCommonShardingInsert.java
+++ src/main/java/org/apache/shardingsphere/benchmark/jmeter/common/jdbc/sharding/JMeterJDBCCommonShardingInsert.java
@@ -9,10 +9,13 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Random;
 
 public class JMeterJDBCCommonShardingInsert extends JMeterBenchmarkBase {
 
     public static DataSource dataSource;
+    public Random r = new Random(1);
+    public int tableCount = Integer.valueOf((String)dbConfig.get("benchmark.table.count")).intValue();
 
     static {
         dataSource = JDBCDataSourceUtil.initDb((String) dbConfig.get("jdbc.benchmark.fullrouting.sharding.ds0.datasource"),
@@ -31,8 +34,8 @@ public class JMeterJDBCCommonShardingInsert extends JMeterBenchmarkBase {
         try {
             connection = dataSource.getConnection();
             String insertSql = (String) sqlConfig.get("common.jdbc.insert.sql");
-            List insertParams = convertParams((List) sqlConfig.get("common.jdbc.insert.values"));
-            insertRecords(connection, insertSql, insertParams);
+            List insertParams = convertParams((List) sqlConfig.get("common.jdbc.insert.values"), r.nextInt(tableCount));
+            JDBCDataSourceUtil.insert(connection, insertSql, insertParams);
         } catch (SQLException e) {
             results.setSuccessful(false);
             e.printStackTrace();
