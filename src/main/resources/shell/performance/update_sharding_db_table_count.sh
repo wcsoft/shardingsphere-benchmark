6c96fa59d007b966de76829a543e4d844e3640a3
wcsoft <wcong86@163.com>
2020-08-17 21:41:44 +0800

    update shell for 4.1.1

--- src/main/resources/shell/performance/update_sharding_db_table_count.sh
+++ src/main/resources/shell/performance/update_sharding_db_table_count.sh
@@ -1,8 +1,6 @@
 chmod -R 777 /export/benchmark/shardingsphere-benchmark/src/main/resources
-db_count = $1 - 1
-table_count = $2 - 1
-echo db_count
-echo table_count
-sed -i "s/{0..[1-9]*}.sbtest\${\0..[1-9]*}/{0..$db_count}.sbtest\${0..$table_count}/g" `grep '{0..[1-9]*}.sbtest\${0..[1-9]*}' -rl /export/benchmark/config-shardingjdbc-singlerouting-sharding-masterslave-encrypt.yaml`
+let db_count=$1-1
+let table_count=$2-1
+sed -i "s/{0..[1-9]*}.sbtest\${\0..[1-9]*}/{0..$db_count}.sbtest\${0..$table_count}/g" `grep '{0..[1-9]*}.sbtest\${0..[1-9]*}' -rl /export/benchmark/shardingsphere-benchmark/src/main/resources/yaml`
 sed -i "s/{id % [1-9]*}/{id % $1}/g" `grep '{id % [1-9]*}' -rl /export/benchmark/shardingsphere-benchmark/src/main/resources/yaml`
 sed -i "s/{k % [0-9]*}/{k % $2}/g" `grep '{k % [0-9]*}' -rl /export/benchmark/shardingsphere-benchmark/src/main/resources/yaml`
