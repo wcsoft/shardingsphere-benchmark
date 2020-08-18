8c9c9aa70c17daa89c37b5695de692f0289ad8a4
wcsoft <wcong86@163.com>
2020-08-18 12:09:17 +0800

    update properties for 4.1.1

--- src/main/resources/shell/performance/update_sharding_db_table_count.sh
+++ src/main/resources/shell/performance/update_sharding_db_table_count.sh
@@ -4,3 +4,4 @@ let table_count=$2-1
 sed -i "s/{0..[1-9]*}.sbtest\${\0..[1-9]*}/{0..$db_count}.sbtest\${0..$table_count}/g" `grep '{0..[1-9]*}.sbtest\${0..[1-9]*}' -rl /export/benchmark/shardingsphere-benchmark/src/main/resources/yaml`
 sed -i "s/{id % [1-9]*}/{id % $1}/g" `grep '{id % [1-9]*}' -rl /export/benchmark/shardingsphere-benchmark/src/main/resources/yaml`
 sed -i "s/{k % [0-9]*}/{k % $2}/g" `grep '{k % [0-9]*}' -rl /export/benchmark/shardingsphere-benchmark/src/main/resources/yaml`
+sed -i "s/benchmark.table.count=[0-9]*/benchmark.table.count=$2/g" `grep 'benchmark.table.count=[0-9]*' -rl /export/benchmark/shardingsphere-benchmark/src/main/resources/config/dbconfig.properties`
--- src/main/resources/shell/prepare/benchmark_update.sh
+++ src/main/resources/shell/prepare/benchmark_update.sh
@@ -1,4 +1,12 @@
+chmod -R 777 /export/benchmark/shardingsphere-benchmark
 cd /export/benchmark/shardingsphere-benchmark
+
+if [ -f ./ss_build_version.log ]; then
+  rm ./ss_build_version.log
+fi
+echo $1 > ./ss_build_version.log
+sleep 2s
+sed -i "s/ss.benchmark.version=[^0-9.]*\([0-9.]*\)\([0-9.]*\)./ss.benchmark.version=$1/g" `grep 'ss.benchmark.version=[^0-9.]*\([0-9.]*\)\([0-9.]*\)' -rl /export/benchmark/shardingsphere-benchmark/src/main/resources/config/benchmark-version.properties`
 
 ss_version=`head -n +1 ./ss_build_version.log`
 
