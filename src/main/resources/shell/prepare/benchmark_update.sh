bca555f39b9895833cabe0b23689284ce7ef581e
wcsoft <wcong86@163.com>
2020-08-25 11:50:03 +0800

    update result path for 4.1.1

--- src/main/resources/shell/prepare/benchmark_update.sh
+++ src/main/resources/shell/prepare/benchmark_update.sh
@@ -11,14 +11,17 @@ ss_version=`head -n +1 ./ss_build_version.log`
 
 if [ $ss_version == 5.0 ];then
   echo "5.0"
+  git checkout master
   git reset --hard origin/master
   git pull origin master
 elif [ $ss_version == 4.1.1 ];then
   echo "4.1.1"
+  git checkout 4.1.1
   git reset --hard origin/4.1.1
   git pull origin 4.1.1
 else
   echo "default version"
+  git checkout master
   git reset --hard origin/master
   git pull origin master
 fi
