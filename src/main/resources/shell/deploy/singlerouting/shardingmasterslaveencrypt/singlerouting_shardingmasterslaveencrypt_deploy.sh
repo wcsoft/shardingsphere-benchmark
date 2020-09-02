7c4225889174070d69f4a35f894c6a747642e599
wcsoft <wcong86@163.com>
2020-09-02 18:44:44 +0800

    update 4.1.1

--- src/main/resources/shell/deploy/singlerouting/shardingmasterslaveencrypt/singlerouting_shardingmasterslaveencrypt_deploy.sh
+++ src/main/resources/shell/deploy/singlerouting/shardingmasterslaveencrypt/singlerouting_shardingmasterslaveencrypt_deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 proxy_work_dir="/home/jenkins"
+proxy_conf_dir="/export/shardingsphere-benchmark/yaml_conf"
 if [ ! -d $proxy_work_dir  ];then
   mkdir -p $proxy_work_dir
 fi
@@ -7,10 +8,7 @@ fi
 cd $proxy_work_dir
 ./apache-shardingsphere-*-sharding-proxy-bin/bin/stop.sh
 sleep 5
-#rm -fr apache-shardingsphere-5.0.0-RC1-SNAPSHOT-sharding-proxy-bin
-#mv sharding-distribution/sharding-proxy-distribution/target/*sharding-proxy-bin.tar.gz .
-#tar zxvf apache-shardingsphere-5.0.0-RC1-SNAPSHOT-sharding-proxy-bin.tar.gz
 rm -f ./apache-shardingsphere-5.0.0-RC1-SNAPSHOT-sharding-proxy-bin/conf/config-*
-cp -f /export/shardingsphere-benchmark/yaml_conf/singlerouting/sharding-masterslave-encrypt/proxy/config-proxy-singlerouting-sharding-masterslave-enc.yaml /export/shardingsphere-benchmark/yaml_conf/server.yaml ./apache-shardingsphere-*-sharding-proxy-bin/conf
+cp -f $proxy_conf_dir/singlerouting/sharding-masterslave-encrypt/proxy/config-proxy-singlerouting-sharding-masterslave-enc.yaml $proxy_conf_dir/server.yaml ./apache-shardingsphere-*-sharding-proxy-bin/conf
 ./apache-shardingsphere-*-sharding-proxy-bin/bin/start.sh
 sleep 10
