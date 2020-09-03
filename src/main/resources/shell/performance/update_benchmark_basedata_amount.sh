df5a6dc3213edcf6e04b08b0345d311d004a2994
wcsoft <wcong86@163.com>
2020-09-03 15:27:01 +0800

    fix error of shell

--- src/main/resources/shell/performance/update_benchmark_basedata_amount.sh
+++ src/main/resources/shell/performance/update_benchmark_basedata_amount.sh
@@ -1,14 +1,14 @@
 benchmark_resource_dir="/export/benchmark/shardingsphere-benchmark/src/main/resources"
 chmod -R 777 $benchmark_resource_dir
 cd $benchmark_resource_dir
-sed -i "s/\"LoopController.loops\">[0-9]*/\"LoopController.loops\">$1/g" `grep '"LoopController.loops">[0-9]*' -rl ./testplan/common/datapreparation/shardingsphere/encrypt/ss-encrypt-createdata-testplan.jmx`
-sed -i "s/\"ThreadGroup.num_threads\">[0-9]*/\"ThreadGroup.num_threads\">$2/g" `grep '"ThreadGroup.num_threads">[0-9]*' -rl ./testplan/common/datapreparation/shardingsphere/encrypt/ss-encrypt-createdata-testplan.jmx`
-sed -i "s/\"LoopController.loops\">[0-9]*/\"LoopController.loops\">$1/g" `grep '"LoopController.loops">[0-9]*' -rl ./testplan/common/datapreparation/shardingsphere/masterslave/ss-masterslave-createdata-testplan.jmx`
-sed -i "s/\"ThreadGroup.num_threads\">[0-9]*/\"ThreadGroup.num_threads\">$2/g" `grep '"ThreadGroup.num_threads">[0-9]*' -rl ./testplan/common/datapreparation/shardingsphere/masterslave/ss-masterslave-createdata-testplan.jmx`
-sed -i "s/\"LoopController.loops\">[0-9]*/\"LoopController.loops\">$1/g" `grep '"LoopController.loops">[0-9]*' -rl ./testplan/common/datapreparation/shardingsphere/sharding/ss-sharding-createdata-testplan.jmx`
-sed -i "s/\"ThreadGroup.num_threads\">[0-9]*/\"ThreadGroup.num_threads\">$2/g" `grep '"ThreadGroup.num_threads">[0-9]*' -rl ./testplan/common/datapreparation/shardingsphere/sharding/ss-sharding-createdata-testplan.jmx`
-sed -i "s/\"LoopController.loops\">[0-9]*/\"LoopController.loops\">$1/g" `grep '"LoopController.loops">[0-9]*' -rl ./testplan/common/datapreparation/shardingsphere/sharding-masterslave-encrypt/ss-sharding-masterslave-encrypt-createdata-testplan.jmx`
-sed -i "s/\"ThreadGroup.num_threads\">[0-9]*/\"ThreadGroup.num_threads\">$2/g" `grep '"ThreadGroup.num_threads">[0-9]*' -rl ./testplan/common/datapreparation/shardingsphere/sharding-masterslave-encrypt/ss-sharding-masterslave-encrypt-createdata-testplan.jmx`
+sed -i "s/\"LoopController.loops\">[0-9]*/\"LoopController.loops\">$1/g" `grep '"LoopController.loops">[0-9]*' -rl ./testplan/common/datapreparation/shardingsphere/encrypt/shardingsphere-encrypt-createdata-testplan.jmx`
+sed -i "s/\"ThreadGroup.num_threads\">[0-9]*/\"ThreadGroup.num_threads\">$2/g" `grep '"ThreadGroup.num_threads">[0-9]*' -rl ./testplan/common/datapreparation/shardingsphere/encrypt/shardingsphere-encrypt-createdata-testplan.jmx`
+sed -i "s/\"LoopController.loops\">[0-9]*/\"LoopController.loops\">$1/g" `grep '"LoopController.loops">[0-9]*' -rl ./testplan/common/datapreparation/shardingsphere/masterslave/shardingsphere-masterslave-createdata-testplan.jmx`
+sed -i "s/\"ThreadGroup.num_threads\">[0-9]*/\"ThreadGroup.num_threads\">$2/g" `grep '"ThreadGroup.num_threads">[0-9]*' -rl ./testplan/common/datapreparation/shardingsphere/masterslave/shardingsphere-masterslave-createdata-testplan.jmx`
+sed -i "s/\"LoopController.loops\">[0-9]*/\"LoopController.loops\">$1/g" `grep '"LoopController.loops">[0-9]*' -rl ./testplan/common/datapreparation/shardingsphere/sharding/shardingsphere-sharding-createdata-testplan.jmx`
+sed -i "s/\"ThreadGroup.num_threads\">[0-9]*/\"ThreadGroup.num_threads\">$2/g" `grep '"ThreadGroup.num_threads">[0-9]*' -rl ./testplan/common/datapreparation/shardingsphere/sharding/shardingsphere-sharding-createdata-testplan.jmx`
+sed -i "s/\"LoopController.loops\">[0-9]*/\"LoopController.loops\">$1/g" `grep '"LoopController.loops">[0-9]*' -rl ./testplan/common/datapreparation/shardingsphere/sharding-masterslave-encrypt/shardingsphere-sharding-masterslave-encrypt-createdata-testplan.jmx`
+sed -i "s/\"ThreadGroup.num_threads\">[0-9]*/\"ThreadGroup.num_threads\">$2/g" `grep '"ThreadGroup.num_threads">[0-9]*' -rl ./testplan/common/datapreparation/shardingsphere/sharding-masterslave-encrypt/shardingsphere-sharding-masterslave-encrypt-createdata-testplan.jmx`
 sed -i "s/\"LoopController.loops\">[0-9]*/\"LoopController.loops\">$1/g" `grep '"LoopController.loops">[0-9]*' -rl ./testplan/common/datapreparation/jdbc/encrypt/jdbc-encrypt-createdata-testplan.jmx`
 sed -i "s/\"ThreadGroup.num_threads\">[0-9]*/\"ThreadGroup.num_threads\">$2/g" `grep '"ThreadGroup.num_threads">[0-9]*' -rl ./testplan/common/datapreparation/jdbc/encrypt/jdbc-encrypt-createdata-testplan.jmx`
 sed -i "s/\"LoopController.loops\">[0-9]*/\"LoopController.loops\">$1/g" `grep '"LoopController.loops">[0-9]*' -rl ./testplan/common/datapreparation/jdbc/masterslave/jdbc-masterslave-createdata-testplan.jmx`
