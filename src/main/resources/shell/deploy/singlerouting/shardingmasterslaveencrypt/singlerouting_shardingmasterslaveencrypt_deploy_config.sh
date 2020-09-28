9e934d7d3f0a6ddaa97a7c1540f451b684af4f73
wcsoft <wcong86@163.com>
2020-09-28 16:34:51 +0800

    update shell

--- src/main/resources/shell/deploy/singlerouting/shardingmasterslaveencrypt/singlerouting_shardingmasterslaveencrypt_deploy_config.sh
+++ src/main/resources/shell/deploy/singlerouting/shardingmasterslaveencrypt/singlerouting_shardingmasterslaveencrypt_deploy_config.sh
@@ -6,7 +6,7 @@ fi
 
 cd $proxy_work_dir
 rm -fr apache-shardingsphere-5.0.0-RC1-SNAPSHOT-sharding-proxy-bin
-mv shardingsphere-distribution/shardingsphere-proxy-distribution/target/apache-shardingsphere-5.0.0-RC1-SNAPSHOT-sharding-proxy-bin.tar.gz .
+mv sharding-distribution/sharding-proxy-distribution/target/apache-shardingsphere-5.0.0-RC1-SNAPSHOT-sharding-proxy-bin.tar.gz .
 tar zxvf apache-shardingsphere-5.0.0-RC1-SNAPSHOT-sharding-proxy-bin.tar.gz
 cp -f ./mysql-connector-java-5.1.47.jar ./shardingsphere-benchmark-1.1-SNAPSHOT.jar ./apache-shardingsphere-5.0.0-RC1-SNAPSHOT-sharding-proxy-bin/lib
 sleep 10
